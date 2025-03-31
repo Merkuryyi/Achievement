17af2b388c9209ad02d3a3c6f1741defe62092e6
Ksenia <nikkseniyu@gmail.com>
2025-03-31 22:13:56 +0700

    31/03

--- achievements/achievements/settings.py
+++ achievements/achievements/settings.py
@@ -68,12 +68,18 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'achievements.wsgi.application'
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'achievement',
+        'USER': 'postgres',
+        'PASSWORD': '62389trewq%Y',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
