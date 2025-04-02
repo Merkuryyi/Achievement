a6ca8ac011589095c510e870e697baa40dd14c50
Ksenia <nikkseniyu@gmail.com>
2025-04-02 21:42:53 +0700

    02/04

--- achievements/achievements_app/models.py
+++ achievements/achievements_app/models.py
@@ -6,3 +6,19 @@ class Achievement(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.core.exceptions import ValidationError
+from django.db import models
+import re
+
+def validate_phone(value):
+    pattern = r'^(\+7|8)[\d\- ]{10,15}$'
+    if not re.match(pattern, value):
+        raise ValidationError('Неверный формат телефона')
+
+class UserProfile(models.Model):
+    phone = models.CharField(
+        max_length=20,
+        validators=[validate_phone],
+        help_text="Формат: +79991234567 или 89991234567"
+    )
